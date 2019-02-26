ff73f221f7aa279d3d06c8b06bacb18ac83f2168
sslmyo24 <sslmyo24@gmail.com>
2019-02-26 12:16:12 +0900

    captcaha  반 정도 구현

--- App/View/captcha.php
+++ App/View/captcha.php
@@ -1,13 +1,54 @@
 <?php
+	function inter_incre ($stand, $max_inter, $val_local, $arr) {
+		for ($i= $max_inter*-1; $i <= $max_inter; $i++) {
+			switch ($val_local) {
+				case 'key':
+					if (array_key_exists($stand + $i, $arr)) return true;
+					break;
+				case 'val':
+					if (in_array($stand + $i, $arr)) return true;
+					break;
+			}
+		}
+	}
 	header('Content-Type: image/jpeg');
-	$captcha_num = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM9784651320';
-	$captcha_num = substr(str_shuffle($captcha_num), 0, 6);
-	$_SESSION['code'] = $captcha_num;
-	$img_width = 150;
-	$img_width = 40;
-	$font_size = 30;
+	$img_width = 400;
+	$img_height = 70;
+	$font_size = 20;
+	$font_url = "C:\\xampp\\htdocs\\Public\\font\\arial.ttf";
 	$image = imagecreate($img_width, $img_height);
 	imagecolorallocate($image, 255, 255, 255);
 	$text_color = imagecolorallocate($image, 0, 0, 0);
-	imagettftext($image, $font_size, 0, 14, 30, $text_color, _PUBLIC."/font/arial.ttf", $captcha_num);
+	$keys = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM9784651320';
+	$len = strlen($keys);
+	$captcha_arr = [];
+	$y_location = [];
+	for ($i = 0; $i < 6; $i++) {
+		$rand_str = $keys[rand(0, $len - 1)];
+		$x = rand($font_size, $img_width - $font_size);
+		$y = rand($font_size, $img_height - $font_size);
+		if (inter_incre($x, $img_width/10, 'key', $captcha_arr)) {
+			$i--;
+			continue;
+		}
+		$captcha_arr[$x] = $rand_str;
+		$y_location[$x] = $y;
+	}
+	ksort($captcha_arr);
+	ksort($y_location);
+	$captcha_str = "";
+	foreach ($captcha_arr as $x => $str) {
+		$captcha_str .= $str;
+		$angle = rand(-90, 90);
+		imagettftext($image, $font_size, $angle, $x, $y_location[$x], $text_color, $font_url, $str);
+	}
+	$_SESSION['code'] = $captcha_str;
+	$line_count = rand(5, 15);
+	for ($i=0; $i < $line_count; $i++) {
+		$x1 = rand(0, $img_width);
+		$x2 = rand(0, $img_width);
+		$y1 = rand(0, $img_height);
+		$y2 = rand(0, $img_height);
+		imageline($image, $x1, $x2, $y1, $y2, $text_color);
+	}
 	imagejpeg($image);
